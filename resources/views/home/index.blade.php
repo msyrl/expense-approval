e0884626a83a3af7c8848be248acc155026c0236
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2020-10-23 20:41:05 +0700

    Update home/index view

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,26 +12,10 @@
                         <div class="col-12">
                             <!-- Default box -->
                             <div class="card">
-                                <div class="card-header">
-                                    <h3 class="card-title">Title</h3>
-
-                                    <div class="card-tools">
-                                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                                            <i class="fas fa-minus"></i>
-                                        </button>
-                                        <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-                                            <i class="fas fa-times"></i>
-                                        </button>
-                                    </div>
-                                </div>
                                 <div class="card-body">
-                                    Start creating your amazing application!
+                                    <h5 class="card-title">Welcome to {{ config('app.long_name') }}</h5>
                                 </div>
                                 <!-- /.card-body -->
-                                <div class="card-footer">
-                                    Footer
-                                </div>
-                                <!-- /.card-footer-->
                             </div>
                             <!-- /.card -->
                         </div>
