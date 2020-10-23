23276dca76844b5155707a33e1094692839f4500
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2020-10-23 19:54:20 +0700

    Update input search size

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -18,10 +18,10 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="row px-1 mb-2">
-                                <div class="col-6 col-sm-4 px-1 mr-auto">
+                                <div class="col col-sm-4 px-1">
                                     <x-list-search />
                                 </div>
-                                <div class="col-auto px-1">
+                                <div class="col-auto px-1 ml-auto">
                                     <x-filter-modal :paginator="$categories">
                                         <x-slot name="body">
                                             <x-sortables :sortables="$sortables" />
