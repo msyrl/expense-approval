ad90950c20ea19cf965ce09d46f7212cb03b63cb
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2021-09-03 07:02:20 +0700

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
-        Role::factory(10)->create();
-
         $this->call([
             PermissionsTableSeeder::class,
+            ApprovalStatusSeeder::class,
         ]);
+        $user = $this->createSuperAdminUser();
+        $role = $this->createSuperAdminRole();
+
+        $user->roles()->sync($role->id);
+        $role->permissions()->sync($this->getPermissionIDs());
+    }
+
+    protected function createSuperAdminUser()
+    {
+        return User::firstOrCreate(
+            [
+                'username' => 'superadmin',
+                'email' => 'superadmin@example.com',
+            ],
+            [
+                'name' => 'Super Admin',
+                'password' => 'secret'
+            ]
+        );
+    }
+
+    protected function createSuperAdminRole()
+    {
+        return Role::firstOrCreate([
+            'name' => 'Super Admin',
+            'slug' => 'Super Admin',
+        ]);
+    }
+
+    protected function getPermissionIDs()
+    {
+        return Permission::all()
+            ->pluck('id')
+            ->all();
     }
 }
